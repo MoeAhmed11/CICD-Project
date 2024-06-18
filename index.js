05c3034cbe2af37d9c8c7ceed09c301958818f86
Mohamed <moh.i.ahmed@hotmail.com>
2024-06-18 13:15:24 +0100

    Final Test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 4');
+    res.send('Hello World! 500');
 });
 
 app.listen(PORT, () => {
